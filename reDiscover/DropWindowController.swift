9d8b9e32e715fc040331949fe6ae70b510c3046a
Teo <m@sartori.dk>
2014-06-30 15:04:39 +0200

    further changes

---
+++ reDiscover/DropWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  DropWindowController.swift
+//  reDiscover
+//
+//  Created by Matteo Sartori on 25/06/14.
+//  Copyright (c) 2014 Teo Sartori. All rights reserved.
+//
+
+import Foundation
+
+class DropWindowController : NSWindowController {
+    override func awakeFromNib()  {
+        println("Window Wake")
+    }
+}
