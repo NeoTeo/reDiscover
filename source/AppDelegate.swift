14733c62ada10265248320d2b3ab64cfd9c408a7
Teo Sartori <m@sartori.dk>
2016-02-03 19:10:20 +0100

    Replace main.m with AppDelegate.swift

---
+++ source/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  reDiscover
+//
+//  Created by teo on 03/02/16.
+//  Copyright © 2016 Teo Sartori. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
